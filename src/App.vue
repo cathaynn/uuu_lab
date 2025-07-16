5b4e522ee02c8ff24a8065a87675a5b8051d5960
cathay nn <cathaynn@gmail.com>
2025-07-16 13:25:07 +0800

    練習component不可以改變pops的值

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <H1>Hello World from SFC(single file component)</H1>
+    <ul>
+        <course-intro id="poop" name="python and oop" duration=35 current="true"></course-intro>
+        <course-intro id="bdpy" name="python and big data" duration=42 current="false"></course-intro>
+    </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    data() {
+        return {
+            courses: [{ id: "poop", name: "python oop", duration: 35 }]
+        }
+    }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#app ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+}
+
+#app li {
+    box-shadow: 0 4px 8px rgba(0, 0, 128, 0.26);
+    margin: 1rem auto;
+    border-radius: 5px;
+    text-align: center;
+    width: 50%;
+    max-width: 40rem;
+}
+
+#app button {
+    font: inherit;
+    cursor: pointer;
+    border: 1px solid #FF0077;
+    background-color: #C0FFEE;
+    color: black;
+
 }
 </style>
---
+++ src/components/CourseIntro.vue
@@ -0,0 +1,29 @@
+<template>
+    <li>
+        <h2>{{ id }}</h2>
+        <button @click="toggleCourseDetail">show detail</button>
+        <ul v-if="detailsVisible">
+            <li>{{ name }}</li>
+            <li>{{ duration }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    props: ["id", "name", "duration", "current"],
+    data() {
+        return {
+            detailsVisible: true
+        }
+    },
+    methods: {
+        toggleCourseDetail() {
+            this.detailsVisible = !this.detailsVisible
+        },
+        toggleCurrent() {
+            // this.current = !this.current;
+        }
+    }
+}
+</script>
